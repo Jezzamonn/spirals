8287dd46e4b7c4f7a961c17cdba1e954efeed9e6
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-07-24 22:36:51 -0700

    Many circles

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,38 @@
 export default class Controller {
 
 	constructor() {
-
+		this.animAmt = 0;
 	}
 
 	update(dt) {
-		// TODO: Some updating logic
+		const period = 2;
+		this.animAmt += dt / peroid;
 	}
 
+	/**
+	 * @param {CanvasRenderingContext2D} context 
+	 */
 	render(context) {
-		// TODO: Some rendering logic
+		const maxDist = 3;
+		const spacing = 500 / (2 * maxDist + 1);
+		const paddingAmt = 0.8;
+
+		for (let x = -maxDist; x <= maxDist; x ++) {
+
+			for (let y = -maxDist; y <= maxDist; y ++) {
+
+				context.beginPath();
+				context.strokeStyle = 'black';
+				context.arc(
+					x * spacing,
+					y * spacing,
+					paddingAmt * spacing / 2,
+					0, 2 * Math.PI
+				)
+				context.stroke();
+
+			}
+		}
 	}
 
 }
